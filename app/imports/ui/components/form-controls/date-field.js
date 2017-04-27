49e36232cacf1a3aa9d22a43008327ccd70d9d5f
Glenn Galvizo <glennjgalvizo@gmail.com>
2017-04-26 18:34:28 -1000

    Had some formatting issues, but just needed to set the position as relative! The javascript is now contained in the js file.

--- app/imports/ui/components/form-controls/date-field.js
+++ app/imports/ui/components/form-controls/date-field.js
@@ -1,5 +1,13 @@
 import { Template } from 'meteor/templating';
 
 Template.Date_Field.onRendered(function onRendered() {
-  this.$('.ui.calendar').checkbox();
+  this.$('.daterangepicker').daterangepicker({
+    singleDatePicker: true,
+    showDropdowns: true,
+    timePicker: true,
+    timePickerIncrement: 30,
+    locale: {
+      format: 'MM/DD/YYYY h:mm A'
+    }
+  });
 });
