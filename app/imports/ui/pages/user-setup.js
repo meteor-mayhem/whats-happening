4d311c098bf90c292611265245bf9f377e62cfa6
Dylan Tokita <dtokita@hawaii.edu>
2017-05-08 17:30:27 -1000

    Form validation not working

--- app/imports/ui/pages/user-setup.js
+++ app/imports/ui/pages/user-setup.js
@@ -2,6 +2,21 @@ import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
 import { categoryList } from './categories.js';
 import { organizationList } from './organizations.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { _ } from 'meteor/underscore';
+import { Profiles, ProfileSchema } from '../../api/profiles/profiles.js';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+const displaySuccessMessage = 'displaySuccessMessage';
+const displayErrorMessages = 'displayErrorMessages';
+
+Template.User_Setup_Page.onCreated(function onCreated() {
+  this.subscribe('Profiles');
+  this.messageFlags = new ReactiveDict();
+  this.messageFlags.set(displaySuccessMessage, false);
+  this.messageFlags.set(displayErrorMessages, false);
+  this.context = ProfileSchema.namedContext('User_Setup_Page');
+});
 
 Template.User_Setup_Page.helpers({
   user: function user() {
@@ -16,5 +31,67 @@ Template.User_Setup_Page.helpers({
     return _.map(organizationList, function makeOrganizationObject(organization) {
       return { label: organization };
     });
+  },
+  successClass() {
+    return Template.instance().messageFlags.get(displaySuccessMessage) ? 'success' : '';
+  },
+  displaySuccessMessage() {
+    return Template.instance().messageFlags.get(displaySuccessMessage);
+  },
+  errorClass() {
+    return Template.instance().messageFlags.get(displayErrorMessages) ? 'error' : '';
+  },
+  fieldError(fieldName) {
+    const invalidKeys = Template.instance().context.invalidKeys();
+    const errorObject = _.find(invalidKeys, (keyObj) => keyObj.name === fieldName);
+    return errorObject && Template.instance().context.keyErrorMessage(errorObject.name);
+  },
+});
+
+Template.User_Setup_Page.events({
+  'submit'(event, instance) {
+    console.log("Press");
+    event.preventDefault();
+    const username = Meteor.user().profile.name;
+    const first = event.target.First_Name.value;
+    const last = event.target.Last_Name.value;
+    const interests = event.target.Interests.value;
+    const organizations = event.target.Organizations.value;
+    const bio = event.target.About_Me.value;
+    const picture = event.target.Profile_Picture.value;
+    const email = event.target.Email.value;
+    const phone = event.target.Phone.value;
+    const events = "";
+    const followers = "";
+    const following = "";
+    const attending = "";
+    const newProfileData = {
+      username,
+      first,
+      last,
+      interests,
+      organizations,
+      bio,
+      events,
+      attending,
+      picture,
+      followers,
+      following,
+    };
+    // Clear out any old validation errors.
+    instance.context.resetValidation();
+    // Invoke clean so that newEventdata reflects what will be inserted.
+    ProfileSchema.clean(newProfileData);
+    // Determine validity.
+    instance.context.validate(newProfileData);
+    if (instance.context.isValid()) {
+      console.log("Valid");
+      const id = Events.insert(newProfileData);
+      instance.messageFlags.set(displayErrorMessages, false);
+      FlowRouter.go(FlowRouter.path('Home_Page', { _id: id }));
+    } else {
+      console.log("Invalid");
+      instance.messageFlags.set(displayErrorMessages, true);
+    }
   },
 });
