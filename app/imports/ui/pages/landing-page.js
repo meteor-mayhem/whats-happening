e62efd9ecde4b4cbb9f3f46675c63a46e68bd46a
Glenn Galvizo <glennjgalvizo@gmail.com>
2017-05-09 19:04:14 -1000

    premature previous commit... this actually removes the admin link, adds CAS functionality to the landing page.
    Also added link text to edit profile page.

--- app/imports/ui/pages/landing-page.js
+++ app/imports/ui/pages/landing-page.js
@@ -1,8 +1,25 @@
 import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
 
+/* eslint-disable no-console */
+
 Template.Landing_Page.events({
   'click .ui.large.green.button.sign.up': function casLogin(event) {
+    event.preventDefault();
+    const callback = function loginCallback(error) {
+      if (error) {
+        console.log(error);
+      }
+    };
+    Meteor.loginWithCas(callback);
+    return false;
+  },
+  /**
+   * Handle the click on the login link.
+   * @param event The click event.
+   * @returns {boolean} False.
+   */
+  'click .cas-login': function casLogin(event) {
     event.preventDefault();
     const callback = function loginCallback(error) {
       if (error) {
